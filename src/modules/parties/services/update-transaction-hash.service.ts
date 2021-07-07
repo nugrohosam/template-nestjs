1985983c85eb90ac503256b6fa7fb33d2d7f5ed7
halimtuhu <halimtuhuprasetyo@gmail.com>
2021-07-07 14:12:57 +0700

    validate transaction hash from with party creator

--- src/modules/parties/services/update-transaction-hash.service.ts
+++ src/modules/parties/services/update-transaction-hash.service.ts
@@ -2,9 +2,11 @@ import {
     Inject,
     NotFoundException,
     UnauthorizedException,
+    UnprocessableEntityException,
 } from '@nestjs/common';
 import { Web3Service } from 'src/infrastructure/web3/web3.service';
 import { PartyModel } from 'src/models/party.model';
+import { UserModel } from 'src/models/user.model';
 import { UpdateDeployedPartyDataRequest } from '../requests/update-transaction-hash.request';
 
 export class UpdateTransactionHashService {
@@ -17,16 +19,23 @@ export class UpdateTransactionHashService {
             throw new UnauthorizedException('Signature not valid.');
     }
 
-    async validateTransactionHash(transactionHash: string): Promise<void> {
+    async validateTransactionHash(
+        transactionHash: string,
+        user: UserModel,
+    ): Promise<void> {
         const transaction = await this.web3Service.getTransaction(
             transactionHash,
         );
 
-        // TODO: need to validate if transactionHash
-        if (!transaction) return;
-        // throw new UnprocessableEntityException(
-        //     'Transaction not found in Network.',
-        // );
+        if (!transaction)
+            throw new UnprocessableEntityException(
+                'Transaction not found in Network.',
+            );
+
+        if (transaction.from !== user.address)
+            throw new UnprocessableEntityException(
+                'Transaction not from correct user.',
+            );
     }
 
     async updateParty(
@@ -39,7 +48,10 @@ export class UpdateTransactionHashService {
         if (!party) throw new NotFoundException('Party not found.');
 
         this.validateSignature(request.memberSignature, party);
-        await this.validateTransactionHash(request.transactionHash);
+        await this.validateTransactionHash(
+            request.transactionHash,
+            await party.$get('creator'),
+        );
         // TODO: validate log event check the transactionHash is belongs to party data
 
         party.address = request.partyAddress;
