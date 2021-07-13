40fbcf8d0032ff9bc332ba59afcb89fb6686dc48
Ananda Daffa Sabila Isliyana <dafpool4@gmail.com>
2021-07-13 10:16:03 +0700

    automation testing on transfer and transaction list

---
+++ test/transactions/transaction-list.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import supertest from 'supertest';
+import { AppModule } from '../../src/app.module';
+import { Validator } from 'jsonschema';
+import schemaSuccess from './resources/transaction-list/list-success.json';
+
+
+const validateJsonSchema = new Validator();
+
+describe('Transaction List Test (GET)', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it('success list transaction', (done) => {
+        return supertest("localhost:3000")
+            .get('/transactions/by-user-id/2f52a365-9573-44b8-856e-a1332a62806c')
+            .expect(200)
+            .then((response) => {
+                const validateResult = validateJsonSchema.validate(
+                    response.body,
+                    schemaSuccess,
+                );
+                expect(validateResult.valid).toBe(true);
+                done();
+            });
+    });
+});
---
+++ test/transactions/transfer.e2e-spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import supertest from 'supertest';
+import { AppModule } from '../../src/app.module';
+import { Validator } from 'jsonschema';
+import schemaSuccess from './resources/transfer/transfer-success.json';
+
+
+const validateJsonSchema = new Validator();
+
+describe('Transfer Test (POST)', () => {
+    let app: INestApplication;
+    let bodyRequest;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+
+        bodyRequest = {
+            from_address: "0xC3f289326CC63ca40e17deecA65553F36b047f6D",
+            to_address: "0xeA23f96a58850d375d43873A572df9C71e14eF52",
+            amount: 5000000000,
+            currency_id: 1,
+            type: "deposit",
+            description: "Initial deposit",
+            transaction_hash: "7d2f09078c82cefc2f63c55b3e19296a33e83e77d14d538368f8271a8ab1cd85",
+            transfer_signature: "adfadslfkadslfkajdsf"
+        };
+    });
+
+    it('success transfer', (done) => {
+        return supertest("localhost:3000")
+            .post('/transactions/transfer')
+            .set('Content-Type', 'application/json')
+            .send(bodyRequest)
+            .expect(201)
+            .then((response) => {
+                const validateResult = validateJsonSchema.validate(
+                    response.body,
+                    schemaSuccess,
+                );
+                expect(validateResult.valid).toBe(true);
+                done();
+            });
+    });
+});
