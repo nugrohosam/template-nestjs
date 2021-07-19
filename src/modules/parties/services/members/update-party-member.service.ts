44efc278e91039dfa6d30efbd59aaa1023650525
halimtuhu <halimtuhuprasetyo@gmail.com>
2021-07-19 11:14:58 +0700

    validate join party transaction

--- src/modules/parties/services/members/update-party-member.service.ts
+++ src/modules/parties/services/members/update-party-member.service.ts
@@ -1,13 +1,18 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Web3Service } from 'src/infrastructure/web3/web3.service';
 import { PartyMemberModel } from 'src/models/party-member.model';
 import { UpdatePartyMemberRequest } from '../../requests/member/update-party-member.request';
 import { GetPartyMemberService } from './get-party-member.service';
+import { joinPartyEvent } from 'src/contracts/JoinPartyEvent.json';
+import { AbiItem } from 'web3-utils';
 
 @Injectable()
 export class UpdatePartyMemberService {
     constructor(
         @Inject(GetPartyMemberService)
         private readonly getPartyMemberService: GetPartyMemberService,
+        @Inject(Web3Service)
+        private readonly web3Service: Web3Service,
     ) {}
 
     private validateSignature(
@@ -25,8 +30,16 @@ export class UpdatePartyMemberService {
         const partyMember = await this.getPartyMemberService.getById(
             partyMemberId,
         );
+        const member = await partyMember.$get('member');
 
         this.validateSignature(request.joinPartySignature, partyMember);
+        await this.web3Service.validateTransaction(
+            request.transactionHash,
+            member.address,
+            joinPartyEvent as AbiItem,
+            2,
+            partyMemberId,
+        );
 
         partyMember.transactionHash = request.transactionHash;
         await partyMember.save();
