288c7fd236a58e9d4a3215e803199627e67c1034
halimtuhu <halimtuhuprasetyo@gmail.com>
2021-07-30 15:26:27 +0700

    fix update proposal status

--- src/modules/parties/services/proposal/reject-proposal.service.ts
+++ src/modules/parties/services/proposal/reject-proposal.service.ts
@@ -1,5 +1,6 @@
 import { Inject } from '@nestjs/common';
 import { Web3Service } from 'src/infrastructure/web3/web3.service';
+import { PartyModel } from 'src/models/party.model';
 import { Proposal } from 'src/models/proposal.model';
 import { UpdateProposalStatusRequest } from '../../requests/proposal/update-proposal-status.request';
 import { GetProposalService } from './get-proposal.service';
@@ -12,6 +13,10 @@ export class RejectProposalService {
         private readonly web3Service: Web3Service,
     ) {}
 
+    generateSignatureMessage(party: PartyModel, proposal: Proposal): string {
+        return `I want to reject a proposal in ${party.name} with title ${proposal.title}`;
+    }
+
     async reject(
         proposalId: string,
         { signature }: UpdateProposalStatusRequest,
@@ -19,12 +24,14 @@ export class RejectProposalService {
         const proposal = await this.getProposalService.getById(proposalId);
         const party = await proposal.$get('party');
         const owner = await party.$get('owner');
+        const message = this.generateSignatureMessage(party, proposal);
 
-        // TODO: need to discuss who exactly will reject the proposal based on party type
+        // TODO: need to removed after testing
+        console.log('message[reject-proposal]: ' + message);
         await this.web3Service.validateSignature(
             signature,
             owner.address,
-            'reject',
+            message,
         );
 
         proposal.rejectedAt = new Date();
