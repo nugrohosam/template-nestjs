f74be9c632f3ce5ce28f1feb55497e92dd7b9851
halimtuhu <halimtuhuprasetyo@gmail.com>
2021-08-02 14:57:03 +0700

    fix bn validation

--- src/modules/parties/services/members/join-party.service.ts
+++ src/modules/parties/services/members/join-party.service.ts
@@ -102,7 +102,7 @@ export class JoinPartyService {
     }
 
     private validateUserInitialDeposit(party: PartyModel, deposit: BN): void {
-        if (deposit.gte(party.minDeposit) || deposit.lte(party.maxDeposit)) {
+        if (deposit.lt(party.minDeposit) || deposit.gt(party.maxDeposit)) {
             throw new UnprocessableEntityException(
                 `Deposit must be between ${party.minDeposit} and ${party.maxDeposit}`,
             );
