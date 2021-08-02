8dad8e147e56f577c9a07fcc4fe3b462da370bdb
halimtuhu <halimtuhuprasetyo@gmail.com>
2021-08-02 21:15:13 +0700

    fix index proposal query

--- src/modules/parties/services/proposal/index-proposal.service.ts
+++ src/modules/parties/services/proposal/index-proposal.service.ts
@@ -1,14 +1,21 @@
-import { Injectable, Query } from '@nestjs/common';
+import { Inject, Injectable, Query } from '@nestjs/common';
 import {
     PaginationResponse,
     SequelizePaginator,
 } from 'sequelize-typescript-paginator';
 import { IndexRequest } from 'src/common/request/index.request';
+import { PartyModel } from 'src/models/party.model';
 import { Proposal } from 'src/models/proposal.model';
 import { IndexProposalResponse } from '../../responses/proposal/index-proposal.response';
+import { GetPartyService } from '../get-party.service';
 
 @Injectable()
 export class IndexProposalService {
+    constructor(
+        @Inject(GetPartyService)
+        private readonly getPartyService: GetPartyService,
+    ) {}
+
     async mapProposal(proposals: Proposal[]): Promise<IndexProposalResponse[]> {
         return Promise.all(
             proposals.map(async (proposal) => {
@@ -18,12 +25,15 @@ export class IndexProposalService {
     }
 
     async fetch(
+        partyId: string,
         @Query() query: IndexRequest,
     ): Promise<PaginationResponse<IndexProposalResponse>> {
+        const party = await this.getPartyService.getById(partyId);
         const { data, meta } = await SequelizePaginator.paginate(Proposal, {
             perPage: query.perPage ?? 10,
             page: query.page ?? 1,
             options: {
+                where: { partyId: party.id },
                 order: [[query.sort ?? 'created_at', query.order ?? 'desc']],
             },
         });
