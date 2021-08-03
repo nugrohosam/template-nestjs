8d73557c596c3a5ffb58698493da64f1ffc2c720
halimtuhu <halimtuhuprasetyo@gmail.com>
2021-08-03 21:43:39 +0700

    fix update transfer transaction hash event validator

--- src/modules/transactions/services/update-transfer.service.ts
+++ src/modules/transactions/services/update-transfer.service.ts
@@ -4,11 +4,19 @@ import { TransactionModel } from 'src/models/transaction.model';
 import { UpdateTransferRequest } from '../requests/update-transfer.request';
 import { depositEvent } from 'src/contracts/DepositEvent.json';
 import { AbiItem } from 'web3-utils';
+import { TransactionTypeEnum } from 'src/common/enums/transaction.enum';
+import { GetUserService } from 'src/modules/users/services/get-user.service';
+import { PartyModel } from 'src/models/party.model';
+import { GetPartyService } from 'src/modules/parties/services/get-party.service';
+import { UserModel } from 'src/models/user.model';
 
 @Injectable()
 export class UpdateTransferService {
     constructor(
         @Inject(Web3Service) private readonly web3Service: Web3Service,
+        @Inject(GetUserService) private readonly getUserService: GetUserService,
+        @Inject(GetPartyService)
+        private readonly getPartyService: GetPartyService,
     ) {}
 
     async update(
@@ -18,13 +26,30 @@ export class UpdateTransferService {
         if (request.transferSignature !== transaction.signature)
             throw new UnauthorizedException('Signature not valid');
 
+        let user: UserModel;
+        let party: PartyModel;
+        if (transaction.type === TransactionTypeEnum.Deposit) {
+            user = await this.getUserService.getUserByAddress(
+                transaction.addressFrom,
+            );
+            party = await this.getPartyService.getByAddress(
+                transaction.addressTo,
+            );
+        }
+
         // TODO: need to discuss with sc about which input index used to be indetifier
         await this.web3Service.validateTransaction(
             request.transactionHash,
             transaction.addressFrom,
             depositEvent as AbiItem,
-            0,
-            transaction.id,
+            [0, 1, 2, 3, 4],
+            [
+                user.address,
+                party.address,
+                user.id,
+                party.id,
+                transaction.amount.toString(),
+            ],
         );
 
         transaction.transactionHash = request.transactionHash;
