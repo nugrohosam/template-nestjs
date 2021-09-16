40c68ae4b50b80c63e3aa9e9db629551533c3b60
Mirza <zanuarmirza@gmail.com>
2021-09-16 20:30:55 +0700

    record swap transaction

--- src/modules/parties/controllers/swap.controller.ts
+++ src/modules/parties/controllers/swap.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+    Query,
+    Headers,
+} from '@nestjs/common';
 import { IApiResponse } from 'src/common/interface/response.interface';
 import { WSService } from 'src/modules/commons/providers/ws-service';
 import { SwapQuoteApplication } from '../applications/swap-quote.application';
@@ -6,10 +14,13 @@ import { SwapQuoteRequest } from '../requests/swap-quote.request';
 import { SwapQuoteResponse } from '../responses/swap-quote.response';
 import { eventSignature as SwapEventSignature } from 'src/contracts/SwapEvent.json';
 import { ILogParams } from '../types/logData';
+import { SwapQuoteTransactionRequest } from '../requests/swap-quote-transaction';
+import { GetSignerService } from 'src/modules/commons/providers/get-signer.service';
 
 @Controller('parties')
 export class SwapController {
     constructor(
+        private readonly getSignerService: GetSignerService,
         private readonly swapQuoteApplication: SwapQuoteApplication,
         private readonly wsService: WSService,
     ) {
@@ -20,6 +31,23 @@ export class SwapController {
             },
         );
     }
+    @Post('/transaction')
+    async transaction(
+        @Headers('Signature') signature: string,
+        @Body() request: SwapQuoteTransactionRequest,
+    ): Promise<IApiResponse<null>> {
+        const user = await this.getSignerService.get(signature, true);
+        const message = await this.swapQuoteApplication.transaction(
+            request,
+            user,
+        );
+
+        return {
+            message: message,
+            data: null,
+        };
+    }
+
     @Get('/:partyId/swap/quote')
     async getQuote(
         @Param('partyId') partyId: string,
--- src/modules/transactions/services/transaction.service.ts
+++ src/modules/transactions/services/transaction.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ITransaction } from 'src/entities/transaction.entity';
 import { TransactionModel } from 'src/models/transaction.model';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class TransactionService {
@@ -23,5 +23,19 @@ export class TransactionService {
         transaction = Object.assign(transaction, data);
         this.repository.save(transaction);
         return transaction;
+    }
+
+    async updateTxHashStatus(
+        txhash: string,
+        status: boolean,
+    ): Promise<UpdateResult> {
+        return this.repository
+            .createQueryBuilder()
+            .update(TransactionModel)
+            .set({ transactionHashStatus: status })
+            .where('transaction_hash = :transaction_hash', {
+                transaction_hash: txhash,
+            })
+            .execute();
     }
 }
