2fd26e79b4e09493d32fb9f17bc8aead1cb87d33
afaf-tech <fatihalfikri5@gmail.com>
2021-09-29 14:19:55 +0700

    fix reviews

--- src/modules/parties/applications/create-party-announcement.ts
+++ src/modules/parties/applications/create-party-announcement.ts
@@ -3,7 +3,7 @@ import { OffchainApplication } from 'src/infrastructure/applications/offchain.ap
 import { Web3Service } from 'src/infrastructure/web3/web3.service';
 import { AnnouncementModel } from 'src/models/announcement.model';
 import { PartyModel } from 'src/models/party.model';
-import { UserModel } from 'src/models/user.model';
+import { GetUserService } from 'src/modules/users/services/get-user.service';
 import { CreateAnnouncementRequest } from '../requests/announcement/create-party-announcement.request';
 import { AnnouncementService } from '../services/announcements/announcement.service';
 
@@ -12,15 +12,16 @@ export class CreatePartyAnnouncementApplication extends OffchainApplication {
     constructor(
         private readonly web3Service: Web3Service,
         private readonly announcementService: AnnouncementService,
+        private readonly getUserService: GetUserService,
     ) {
         super();
     }
 
     async call(
-        user: UserModel,
         party: PartyModel,
         request: CreateAnnouncementRequest,
     ): Promise<AnnouncementModel> {
+        const user = await this.getUserService.getUserById(party.ownerId);
         await this.web3Service.validateSignature(
             request.signature,
             user.address,
