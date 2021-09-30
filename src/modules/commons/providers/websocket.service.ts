9c480e0ada7c0b96a425338ff0d180de76c58813
halimtuhu <halimtuhuprasetyo@gmail.com>
2021-09-30 21:16:07 +0700

    add cron job to ping infura websocket every minute 0 adn 30

--- src/modules/commons/providers/websocket.service.ts
+++ src/modules/commons/providers/websocket.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { config } from 'src/config';
 import WebSocket from 'ws';
 
@@ -6,13 +7,17 @@ import WebSocket from 'ws';
 export class WSService {
     // wss://echo.websocket.org is a test websocket server
     private ws: WebSocket;
-    private listEventHandler: Record<string, (any) => void> = {};
+    private listEventHandler: Record<string, (arg0: any) => Promise<void>> = {};
+
     constructor() {
         this.ws = new WebSocket(config.web3.websocketProvider);
         this.ws.on('message', (message) => {
             this.onMessage(message);
         });
-        this.ws.on('error', (err) => console.log('err', err));
+        this.ws.on('error', (err) => {
+            console.log('=> WebSocket Error:');
+            console.log(err);
+        });
         this.ws.on('open', () => {
             Object.keys(this.listEventHandler).forEach((key) => {
                 console.log('register topic:', key);
@@ -23,15 +28,19 @@ export class WSService {
                     params: ['logs', { topics: [key] }],
                 });
             });
+            this.ping();
+        });
+        this.ws.on('pong', (data: Buffer) => {
+            console.log('=> WebSocket Pong: ' + data.toString());
         });
     }
 
     // eslint-disable-next-line @typescript-eslint/ban-types
-    send(data: Object) {
+    send(data: Object): void {
         this.ws.send(JSON.stringify(data));
     }
 
-    async onMessage(message: WebSocket.Data) {
+    async onMessage(message: WebSocket.Data): Promise<void> {
         const obj = JSON.parse(message.toString());
         if (obj.params) {
             await this.listEventHandler[obj.params.result.topics[0]](
@@ -39,7 +48,17 @@ export class WSService {
             );
         }
     }
-    registerHandler(topic, handler) {
+
+    registerHandler(
+        topic: string,
+        handler: (arg0: any) => Promise<void>,
+    ): void {
         this.listEventHandler[topic] = handler;
+    }
+
+    @Cron('0 0,30 * * * *')
+    ping(): void {
+        const topics = Object.keys(this.listEventHandler);
+        this.ws.ping(`${topics.length} topics registered on ${this.ws.url}`);
     }
 }
--- src/modules/parties/scheduler/party-gain-scheduler.service.ts
+++ src/modules/parties/scheduler/party-gain-scheduler.service.ts
@@ -6,13 +6,15 @@ import { PartyGainService } from '../services/party-gain/party-gain.service';
 @Injectable()
 export class PartyGainSchedulerService {
     private readonly logger = new Logger(PartyGainSchedulerService.name);
+
     constructor(private readonly partyGainService: PartyGainService) {}
+
     @Cron('0 0 * * * *')
-    handlerTask() {
-        if (!config.scheduler.partyGain) {
-            return;
-        }
-        this.logger.debug('------ PROCESSING PARTY_GAIN SERVICE');
+    handlerTask(): void {
+        if (!config.scheduler.partyGain) return;
+
+        this.logger.debug('------ PROCESSING PARTY_GAIN SERVICE ------');
         this.partyGainService.updatePartiesGain();
+        this.logger.debug('------ END PROCESSING PARTY_GAIN SERVICE ------');
     }
 }
