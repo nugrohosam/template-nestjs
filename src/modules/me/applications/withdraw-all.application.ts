85b266072c3e12fd3769d4279d3c66c0e7746cfb
afaf-tech <fatihalfikri5@gmail.com>
2021-12-23 14:53:53 +0700

    fix withdraw all

--- src/modules/me/applications/withdraw-all.application.ts
+++ src/modules/me/applications/withdraw-all.application.ts
@@ -76,7 +76,10 @@ export class WithdrawAllApplication {
                     .divn(config.calculation.maxPercentage);
 
                 let swapResponse: ISwap0xResponse = null;
-                if (token.address !== defaultToken.address) {
+                if (
+                    token.address !== defaultToken.address &&
+                    !balance.isZero()
+                ) {
                     const { data, err } = await this.swapQuoteService.getQuote(
                         defaultToken.address,
                         token.address,
