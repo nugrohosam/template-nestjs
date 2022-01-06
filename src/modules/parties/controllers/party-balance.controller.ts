4f88189ca0d4ec311adf6174e68c6e2c8efdf73b
nugrohosam-dot <nugroho@dot-indonesia.com>
2022-01-06 15:09:37 +0700

    Add Endpoint | Balance in parties

---
+++ src/modules/parties/controllers/party-balance.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { IApiResponse } from 'src/common/interface/response.interface';
+import { IndexRequest } from 'src/common/request/index.request';
+import { IndexPartyTokenApplication } from '../applications/index-party-token.application';
+import { BalanceResponse } from '../responses/balance/balance.response';
+
+@Controller('parties/:partyId/balance')
+export class PartyBalanceController {
+    constructor(
+        private readonly indexPartyTokenApplication: IndexPartyTokenApplication,
+    ) {}
+
+    @Get()
+    async index(
+        @Param('partyId') partyId: string,
+        @Query() request: IndexRequest,
+    ): Promise<IApiResponse<BalanceResponse>> {
+        const { data, meta } = await this.indexPartyTokenApplication.fetch(
+            partyId,
+            request,
+        );
+
+        const balance = new BalanceResponse;
+        balance.total = 1000;
+        
+        return {
+            message: 'Success get party tokens',
+            data: balance,
+            meta,
+        };
+    }
+}
